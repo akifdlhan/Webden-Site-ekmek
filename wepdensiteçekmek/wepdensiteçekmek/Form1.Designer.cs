2df55f2c9c4d53213978803ac2ad02e55ab0421a
akifdlhan <112507694+akifdlhan@users.noreply.github.com>
2022-09-07 21:35:49 +0300

    Add files via upload

---
+++ wepdensiteçekmek/wepdensiteçekmek/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace wepdensiteçekmek
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(2, -1);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(820, 446);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "FOTOMAÇ",
+            "SAHİBİNDEN",
+            "KARİYER.NET",
+            "VAN OBS",
+            "NETFLİX"});
+            this.comboBox1.Location = new System.Drawing.Point(851, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1073, 446);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ wepdensiteçekmek/wepdensiteçekmek/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wepdensiteçekmek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                webBrowser1.ScriptErrorsSuppressed = true;
+                webBrowser1.Navigate("https://www.fotomac.com.tr");
+            }else if(comboBox1.SelectedIndex == 1)
+            {
+                webBrowser1.ScriptErrorsSuppressed = true;
+                webBrowser1.Navigate("https://www.sahibinden.com");
+            }else if (comboBox1.SelectedIndex == 2)
+            {
+                webBrowser1.ScriptErrorsSuppressed = true;
+                webBrowser1.Navigate("https://www.kariyer.net");
+
+            }else if (comboBox1.SelectedIndex == 3)
+            {
+                webBrowser1.ScriptErrorsSuppressed = true;
+                webBrowser1.Navigate("https://obs.yyu.edu.tr/login.aspx");
+            }else if(comboBox1.SelectedIndex == 4)
+            {
+                webBrowser1.ScriptErrorsSuppressed = true;
+                webBrowser1.Navigate("https://www.netflix.com/browse");
+
+            }
+        }
+
+    }
+}
